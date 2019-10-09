6911f07b66ed65b677fbcb43f2f8c92c30ff6af5
Aaron-Fuller <aaron.fuller@protonmail.com>
2019-10-09 12:10:15 -0400

    const [value, setValue] = useState(); to finish step 2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import React, {useState} from "react";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [valueHome, setValueHome] = useState();
+  const [valueAway, setValueAway] = useState();
 
   return (
     <div className="container">
